83d91775e5ee584e2f10b9b832e2a88aad70f803
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-14 20:20:33 +0800

    [Practice07] [Joseph] Model: Created a model for Student which inherits Person class and added klass attribute

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,22 @@
 package practice07;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public int getKlassNumber() {
+        return klass.getNumber();
+    }
+
+    public String introduce() {
+        return super.introduce() + " I am a Student. I am at Class " + klass.getNumber() + ".";
+    }
 }
