023e96d498c904daaecc7b7bac2d5c87625c1804
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-14 20:18:41 +0800

    [Practice07] [Joseph] Model: Created a model for Klass added number and displayName attributes

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,23 @@
 package practice07;
 
 public class Klass {
+    private int number;
+    private String displayName;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Klass(int number, String displayName) {
+        this.number = number;
+        this.displayName = displayName;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
 }
