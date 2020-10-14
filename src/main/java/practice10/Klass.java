6efed3776f93fe14738abfca7303de087ba20700
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-14 22:58:59 +0800

    [Practice10] [Joseph] Model: Created a model for Klass

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,46 @@
 package practice10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Klass> schoolClass = new ArrayList<>();
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Klass(int number, String displayName) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        if (schoolClass.contains(student.getKlass())) {
+            this.leader = student;
+            return;
+        }
+        System.out.print("It is not one of us.\n");
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student) {
+        schoolClass.add(student.getKlass());
+    }
+
+    public static boolean isIn(List<Klass> classes, Student student) {
+        return classes.contains(student.getKlass());
+    }
 }
