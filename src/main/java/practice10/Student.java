9db6ca652cb72842b609308833b8cc2777b8a41e
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-14 23:00:00 +0800

    [Practice10] [Joseph] Model: Created a model for Student

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,36 @@
 package practice10;
 
-public class Student {
+import java.util.Optional;
+
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public int getKlassNumber() {
+        return klass.getNumber();
+    }
+
+    public String introduce() {
+        boolean isClassLeader = Optional.ofNullable(klass.getLeader())
+                .map(Person::getName)
+                .filter(s -> super.getName().equals(s))
+                .isPresent();
+
+        String classLeader = isClassLeader
+                ? "I am Leader of Class "
+                : "I am at Class ";
+
+        return super.introduce()
+                + " I am a Student. "
+                + classLeader
+                + klass.getNumber() + ".";
+    }
 }
