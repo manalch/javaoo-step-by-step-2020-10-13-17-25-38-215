0ac5f9327836b0f629f19e97979a74e9d386ea6b
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-14 20:21:36 +0800

    [Practice07] [Joseph] Model: Created a model for Teacher which inherits Person class and added klass attribute. Included also the functions introduce and introduceWith

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,38 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        String classTeacher = klass == null
+                ? " I am a Teacher. I teach No Class."
+                : " I am a Teacher. I teach Class " + klass.getNumber() + ".";
+        return super.introduce().concat(classTeacher);
+    }
+
+    public String introduceWith(Student student) {
+        String teachStudent = klass.getNumber() == student.getKlassNumber()
+                ? "I teach"
+                : "I don't teach";
+
+        return super.introduce()
+                + " I am a Teacher. "
+                + teachStudent
+                + " "
+                + student.getName()
+                + ".";
+    }
 }
