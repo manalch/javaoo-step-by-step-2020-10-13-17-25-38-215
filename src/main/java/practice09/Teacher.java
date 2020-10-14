f91cade0c189eaf7956458cca0e28da568154698
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-14 21:30:36 +0800

    [Practice09] [Joseph] Model: Created a model for Teacher which inherits Person class.

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,38 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        String classTeacher = klass == null
+                ? " I am a Teacher. I teach No Class."
+                : " I am a Teacher. I teach Class " + klass.getNumber() + ".";
+        return super.introduce().concat(classTeacher);
+    }
+
+    public String introduceWith(Student student) {
+        String teachStudent = klass.getNumber() == student.getKlassNumber()
+                ? "I teach"
+                : "I don't teach";
+
+        return super.introduce()
+                + " I am a Teacher. "
+                + teachStudent
+                + " "
+                + student.getName()
+                + ".";
+    }
 }
